d009129eb7cf8c6931855f39cea248c0c8472c27
Grant Phillips <grantphillips125@gmail.com>
2025-10-28 17:05:20 -0400

    Basic Springboot / Maven setup

---
+++ SvelteAmazin/src/test/java/com/amazin/svelteamazin/SvelteAmazinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazin.svelteamazin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvelteAmazinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
