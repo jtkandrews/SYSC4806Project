11828324e3512c01dee17d9eb69ecbd615934989
tharsan18 <tharsan18@hotmail.ca>
2025-11-02 18:47:02 -0500

    Import csv file to JPA persistence database, add more books to dataset, Change Book description to genre. Add sorting to Book API

--- SvelteAmazin/src/main/java/com/amazin/svelteamazin/SvelteAmazinApplication.java
+++ SvelteAmazin/src/main/java/com/amazin/svelteamazin/SvelteAmazinApplication.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Bean;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class SvelteAmazinApplication {
