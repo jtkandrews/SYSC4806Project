d12a90fcbc11b0d0bdaeb8d4abb8a694aa59ec19
zdredge <z.dredge32@gmail.com>
2025-11-02 14:00:51 -0500

    Updated test cases to account for new imageURL field in Book

--- SvelteAmazin/src/test/java/model_test/BookStoreTest.java
+++ SvelteAmazin/src/test/java/model_test/BookStoreTest.java
@@ -27,7 +27,7 @@ public class BookStoreTest {
     @Test
     void testAddAndGetBookByIsbn() {
         BookStore store = new BookStore();
-        Book b = new Book("978-0-111111-11-1", "Test Title", "Author Name", "Publisher", "Desc", 19.99, 5);
+        Book b = new Book("978-0-111111-11-1", "Test Title", "Author Name", "Publisher", "Desc", 19.99, 5, "http://example.com/test.jpg");
         store.getAllBooks().add(b);
 
         Optional<Book> found = store.getBookByIsbn("978-0-111111-11-1");
@@ -39,7 +39,8 @@ public class BookStoreTest {
                 () -> assertEquals("Author Name", got.getAuthor()),
                 () -> assertEquals("Publisher", got.getPublisher()),
                 () -> assertEquals(19.99, got.getPrice(), 1e-6),
-                () -> assertEquals(5, got.getInventory())
+                () -> assertEquals(5, got.getInventory()),
+                () -> assertEquals("http://example.com/test.jpg", got.getImageUrl())
         );
     }
 
@@ -47,9 +48,9 @@ public class BookStoreTest {
     void testSearchBooksCaseInsensitiveAndLiveList() {
         BookStore store = new BookStore();
 
-        Book b1 = new Book("1", "Java Programming", "Alice Example", "Pub", "Desc", 10.0, 1);
-        Book b2 = new Book("2", "Advanced Java", "Bob Example", "Pub", "Desc", 15.0, 2);
-        Book b3 = new Book("3", "Python Guide", "Carol Example", "Pub", "Desc", 12.0, 3);
+        Book b1 = new Book("1", "Java Programming", "Alice Example", "Pub", "Desc", 10.0, 1, "img1.jpg");
+        Book b2 = new Book("2", "Advanced Java", "Bob Example", "Pub", "Desc", 15.0, 2, "img2.jpg");
+        Book b3 = new Book("3", "Python Guide", "Carol Example", "Pub", "Desc", 12.0, 3, "img3.jpg");
 
         store.getAllBooks().addAll(List.of(b1, b2, b3));
 
@@ -61,6 +62,7 @@ public class BookStoreTest {
         List<Book> aliceResults = store.searchBooks("alice");
         assertEquals(1, aliceResults.size());
         assertEquals("Java Programming", aliceResults.get(0).getTitle());
+        assertEquals("img1.jpg", aliceResults.get(0).getImageUrl());
 
         // verify the returned list is affected by mutating the live list
         store.getAllBooks().remove(b1);
@@ -68,4 +70,3 @@ public class BookStoreTest {
         assertEquals(1, javaAfterRemove.size(), "removing from the live list should change search results");
     }
 }
-
--- SvelteAmazin/src/test/java/model_test/BookTest.java
+++ SvelteAmazin/src/test/java/model_test/BookTest.java
@@ -17,6 +17,7 @@ public class BookTest {
         book.setDescription("Sample description");
         book.setPrice(19.99);
         book.setInventory(5);
+        book.setImageUrl("http://example.com/image.jpg");
 
         assertEquals("978-0-123456-47-2", book.getIsbn());
         assertEquals("Test Title", book.getTitle());
@@ -25,12 +26,13 @@ public class BookTest {
         assertEquals("Sample description", book.getDescription());
         assertEquals(19.99, book.getPrice(), 1e-6);
         assertEquals(5, book.getInventory());
+        assertEquals("http://example.com/image.jpg", book.getImageUrl());
     }
 
     @Test
     void testAllArgsConstructor() {
         Book book = new Book("978-1-234567-89-7", "Another Title", "A. Author", "Some Publisher",
-                "Another description", 0.0, 0);
+                "Another description", 0.0, 0, "http://example.com/another.jpg");
 
         assertAll("book",
                 () -> assertEquals("978-1-234567-89-7", book.getIsbn()),
@@ -39,18 +41,20 @@ public class BookTest {
                 () -> assertEquals("Some Publisher", book.getPublisher()),
                 () -> assertEquals("Another description", book.getDescription()),
                 () -> assertEquals(0.0, book.getPrice(), 1e-6),
-                () -> assertEquals(0, book.getInventory())
+                () -> assertEquals(0, book.getInventory()),
+        () -> assertEquals("http://example.com/another.jpg", book.getImageUrl())
         );
     }
 
     @Test
     void testMutability() {
-        Book book = new Book("isbn", "title", "author", "pub", "desc", 1.0, 10);
+        Book book = new Book("isbn", "title", "author", "pub", "desc", 1.0, 10, "http://example.com/img.png");
         book.setPrice(2.5);
         book.setInventory(7);
 
         assertEquals(2.5, book.getPrice(), 1e-6);
         assertEquals(7, book.getInventory());
+        assertEquals("http://example.com/img.png", book.getImageUrl());
     }
 }
 
