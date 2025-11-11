c4fc5a0d001536885bddab4e4eec721d7365a59c
zdredge <z.dredge32@gmail.com>
2025-11-11 16:47:01 -0500

    Updated individual book view to show genre and description
    Updated Book model to properly reflect genre and description attributes. Associated tests also updated.

--- SvelteAmazin/src/main/java/com/amazin/svelteamazin/model/Book.java
+++ SvelteAmazin/src/main/java/com/amazin/svelteamazin/model/Book.java
@@ -11,6 +11,7 @@ public class Book {
     @Column(nullable = false) private String author;
     private String publisher;
     @Column(length = 4000) private String genre;
+    @Column(length = 4000) private String description;
     @Column(nullable = false) private double price;
     @Column(nullable = false) private int inventory;
     private String imageUrl;
@@ -30,11 +31,25 @@ public class Book {
         this.imageUrl = imageUrl;
     }
 
+    public Book(String isbn, String title, String author, String publisher,
+                String genre, String description, double price, int inventory, String imageUrl) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.genre = genre;
+        this.description = description;
+        this.price = price;
+        this.inventory = inventory;
+        this.imageUrl = imageUrl;
+    }
+
     public String getIsbn() { return isbn; }
     public String getTitle() { return title; }
     public String getAuthor() { return author; }
     public String getPublisher() { return publisher; }
-    public String getDescription() { return genre; }
+    public String getGenre() { return genre; }
+    public String getDescription() { return description; }
     public double getPrice() { return price; }
     public int getInventory() { return inventory; }
     public String getImageUrl() { return imageUrl; }
@@ -44,7 +59,8 @@ public class Book {
     public void setTitle(String title) { this.title = title; }
     public void setAuthor(String author) { this.author = author; }
     public void setPublisher(String publisher) { this.publisher = publisher; }
-    public void setDescription(String genre) { this.genre = genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public void setDescription(String description) { this.description = description; }
     public void setPrice(double price) { this.price = price; }
     public void setInventory(int inventory) { this.inventory = inventory; }
     public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
